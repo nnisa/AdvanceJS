7bec382e299a7af31863784bea428e5f3831b43a
nnisa <nisanoshin@gmail.com>
2017-02-02 19:41:34 -0500

    learning about arrays

--- Intro/script.js
+++ Intro/script.js
@@ -1,8 +1,23 @@
-console.log('hello world')
+// console.log('hello world')
 
-//Loosely typed 
-var name = 'dude'
-var age = 25
-var height = 6.2
+// //Loosely typed 
+// var name = 'dude'
+// var age = 25
+// var height = 6.2
 
-console.log(name, age,height)
+// console.log(name, age,height)
+
+// var hobbies = ['music', 'beer', 'gifs']
+
+// console.log(hobbies)
+
+
+var Bubble = function (posX, posY, size){
+
+this.a = posX
+this.b = posY
+this.c = 0.5
+}
+
+var myBubble = new Bubble(0, 0, 20)
+console.log(myBubble.c)
