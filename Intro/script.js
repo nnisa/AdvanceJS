895dd33702199c34bfb456719d1e7a2be9fc2ecd
nnisa <nisanoshin@gmail.com>
2017-02-02 21:00:38 -0500

    playing with array lists

--- Intro/script.js
+++ Intro/script.js
@@ -1,71 +1,138 @@
-// // console.log('hello world')
+// // // console.log('hello world')
 
-// // //Loosely typed 
-// // var name = 'dude'
-// // var age = 25
-// // var height = 6.2
+// // // //Loosely typed 
+// // // var name = 'dude'
+// // // var age = 25
+// // // var height = 6.2
 
-// // console.log(name, age,height)
+// // // console.log(name, age,height)
 
-// // var hobbies = ['music', 'beer', 'gifs']
+// // // var hobbies = ['music', 'beer', 'gifs']
 
-// // console.log(hobbies)
+// // // console.log(hobbies)
 
 
-// var Bubble = function (posX, posY, size){
+// // var Bubble = function (posX, posY, size){
 
-// this.a = posX
-// this.b = posY
-// this.c = 0.5
-// }
-
-// // Bubble.prototype.update = function(newPosX, newPosY, acc){
-// // 	this.posX = newPosX
-// // 	//.................
+// // this.a = posX
+// // this.b = posY
+// // this.c = 0.5
 // // }
 
-// Bubble.prototype = {
-// 	update: function(newPosX, newPosY, acc){
-// 	this.posX = newPosX
-// 	//.................
+// // // Bubble.prototype.update = function(newPosX, newPosY, acc){
+// // // 	this.posX = newPosX
+// // // 	//.................
+// // // }
+
+// // Bubble.prototype = {
+// // 	update: function(newPosX, newPosY, acc){
+// // 	this.posX = newPosX
+// // 	//.................
+// // 	}
+// // 	display: function(){
+// // 		console.log(this.posX)
+// // 	}
+// // }
+// // var myBubble = new Bubble(0, 0, 20)
+// // myBubble.update(10, 20, 30)
+// // console.log(myBubble.c)
+
+
+
+
+// // function makeCoffee(beans, water) {
+// // 	grindBeans(beans)
+// // 	addWater(water)
+// // }
+
+// // function grindBeans(beans){
+
+// // }
+
+// // function addWater(water){
+// // 	//adds water to the mix
+// // }
+
+
+// var name = 'luke skywalker'
+// // console.log(name)
+// // console.log(name.length)
+
+// var height = 6.5
+// var rounded = Math.round(height)
+
+// var isRad = 'true'
+// // console.log(typeof(isRad))
+
+// var elem = document.getElementById('para')
+// console.log(elem.innerHTML)
+
+// var courses = ['thesis', 'webjs', 'pcomp']
+
+
+// for (var i=0; i < courses.length; i++){
+// 	console.log(courses[i])
+// }
+
+// var thesis = {
+// 	credits: 6;
+// 	type: 'core',
+// 	register: function(){
+// 		console.log(this.credits)
 // 	}
-// 	display: function(){
-// 		console.log(this.posX)
-// 	}
 // }
-// var myBubble = new Bubble(0, 0, 20)
-// myBubble.update(10, 20, 30)
-// console.log(myBubble.c)
+// thesis.register()
+
+
+////////////////////////////////////////////
+
+var inventory = ['chair', 'table', 'board']
+userCredit = true 
+
+
+while (inventory.length !=0){
+	sell()
+	console.log(inventory)
+}
+
+for (var i = 0; i < inventory.length; i++){
+	sell()
+	console.log(inventory)
+}
+
+function sell() {
+	if (inventory.length > 0 && userCredit){
+	console.log('SALE')
+	console.log(inventory.pop())
+} else {
+	console.log('no sale')
+	}
+}
 
 
 
 
-// function makeCoffee(beans, water) {
-// 	grindBeans(beans)
-// 	addWater(water)
-// }
-
-// function grindBeans(beans){
-
-// }
-
-// function addWater(water){
-// 	//adds water to the mix
-// }
 
 
-var name = 'luke skywalker'
-// console.log(name)
-// console.log(name.length)
 
-var height = 6.5
-var rounded = Math.round(height)
 
-var isRad = 'true'
-// console.log(typeof(isRad))
 
-var elem = document.getElementById('para')
-console.log(elem.innerHTML)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
