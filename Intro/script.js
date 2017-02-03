89eaf6649038ccd68b05067201fe8eca8e81018f
nnisa <nisanoshin@gmail.com>
2017-02-02 19:48:33 -0500

    prototype update

--- Intro/script.js
+++ Intro/script.js
@@ -19,5 +19,41 @@ this.b = posY
 this.c = 0.5
 }
 
+// Bubble.prototype.update = function(newPosX, newPosY, acc){
+// 	this.posX = newPosX
+// 	//.................
+// }
+
+Bubble.prototype = {
+	update: function(newPosX, newPosY, acc){
+	this.posX = newPosX
+	//.................
+	}
+	display: function(){
+		console.log(this.posX)
+	}
+}
+
+
+
+
 var myBubble = new Bubble(0, 0, 20)
+myBubble.update(10, 20, 30)
 console.log(myBubble.c)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
